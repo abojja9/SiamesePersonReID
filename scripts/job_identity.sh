6517231e21ea0652206d25223bc529e2631302b5
ABHISHAKE KUMAR BOJJA <abojja9@beluga2.int.ets1.calculquebec.ca>
2020-04-20 14:09:49 -0400

    Updated scripts

--- scripts/job_identity.sh
+++ scripts/job_identity.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# source activate LSD-seg_v1
-# python main.py --dataroot '/home/abojja/stn_gan/test-time-domain-adaptation/synthia-seg/data' --checkpoint "/data/scratch/abojja/sept_2019/checkpoint"  --network='segnet' --continue_train=0  --phase=train --height=320 --width=640 --batch_size=8 
 source ~/tf_env/bin/activate
 
 python3 train_net.py \
