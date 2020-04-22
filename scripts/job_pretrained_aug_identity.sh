a0e8173cde07458477338a22d53f2be1b92c42e3
ABHISHAKE KUMAR BOJJA <abojja9@beluga4.int.ets1.calculquebec.ca>
2020-04-22 02:12:43 -0400

    Added pretrained scripts

---
+++ scripts/job_pretrained_aug_identity.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source ~/tf_env/bin/activate
+
+python3 train_net.py \
+--batch_size=32 \
+--continue_train=1 \
+--lr=5e-5 \
+--summary_intv=1000 \
+--validation_intv=5000 \
+--network_type=pretrained \
+--data_augment=1 \
+--loss=identity \
+--tf_record_dir=./tf_record_dir_100k
